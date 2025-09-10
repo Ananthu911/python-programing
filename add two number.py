3891d157e60eabd4bc2837a8e18ae1093b86e9bc
Ananthu911 <ananthu0705@gmail.com>
2025-09-10 17:49:23 +0530

    Add files via upload

---
+++ add two number.py
@@ -0,0 +1,5 @@
+def add():
+    a=int(input("enter first number:"))
+    b=int(input("enter second number:"))
+    print("sum",a+b)
+add()
---
+++ dec.py
@@ -0,0 +1,7 @@
+d={}
+elements=int(input("enter a number:"))
+for i in range(elements):
+    key=input("enter key:")
+    value=input("enter value:")
+    d[key]=value
+print(d)
